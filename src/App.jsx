6c6ff5e338ba1648c2d039b75ed816ceb8ccd6f2
Michael Parchment <85087548+mparchment@users.noreply.github.com>
2024-08-15 19:21:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
           <h2>Experience</h2>
           <div className="role-list">
             <RoleCard 
-              date="2024 — Present"
+              date="2023 — Present"
               title="Software Engineer · DocAI"
               description="At DocAI, a telehealth startup connecting users to providers through a chat interface, I spearheaded centralized logging with Elasticsearch, Logstash, and Docker, and collaborated with other developers to build on the frontend using React, Redux, and TypeScript. I was also responsible for testing on the the Django backend, including mocking all external API calls. Additionally, I contributed to writing extensive documentation for our software architechture."
               techList={["JavaScript", "TypeScript", "Python", "Django", "Azure", "Elasticsearch", "Logstash", "Docker"]}
@@ -50,7 +50,7 @@ function App() {
               image={DocAIImage}
             />
             <RoleCard 
-              date="2023 — 2024"
+              date="2022 — 2023"
               title="Web Developer · MCC"
               description="At MCC, I maintained and enhanced multiple WordPress websites with  continuous uptime and up-to-date content. I developed and implemented custom WordPress themes and mobile-friendly templates, driving increased website traffic, lead generation, and alignment with site design and branding goals. Through performance optimization for speed, scalability, and improved search engine visibility, I achieved reduced page load times and higher user engagement, resulting in increased organic traffic. I also provided technical support and training to staff, ensuring proficient website usage."
               techList={["WordPress", "PHP", "HTML", "CSS", "Google Analytics"]}
